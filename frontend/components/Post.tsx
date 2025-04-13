eef44faced48b38e483622114fc3e3a96f04ea37
Hiten Gal <hitengal@gmail.com>
2025-04-13 11:33:43 +0530

    fix

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -1,5 +1,6 @@
 import { Post as PostType } from "@/lib/types";
 import Link from "next/link";
+import TimeAgo from "./TImeAgo";
 
 export default function Post({ post }: { post: PostType }) {
     return (
@@ -7,7 +8,7 @@ export default function Post({ post }: { post: PostType }) {
             <h2 className="text-xl font-bold mb-2"><Link href={`/blog/${post.slug}`}>{post.title}</Link></h2>
             <p className="text-gray-700">{post.content.substr(0,100)}</p>
             <span className="text-xs text-gray-500">{post.author}</span>
-            <span className="text-xs text-gray-500 block">Posted {new Date(post.created_at).toLocaleString()}</span>
+            <span className="text-xs text-gray-500 block">Posted <TimeAgo date={post.created_at} /></span>
         </div>
     )
 }
